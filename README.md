218e8949c293e1a749bb224640cd1c2b2ab51c0b
Stian Gudmundsen Høiland <stianghoiland@gmail.com>
2025-02-14 23:07:58 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# mute
+# ![mute](https://github.com/user-attachments/assets/197bb48a-923e-44ac-bd45-b807e3ef28a9) mute
+
 mute is a super lightweight and robust utility that gives you a system-wide hotkey to mute and unmute your microphone(s).
+
+## Why?
+
+Juggling different apps' hotkeys to mute and unmute yourself and making sure that the various apps are in the same mute state is tedious and error-prone. Think video or voice calls, streaming, and gaming. mute gives you a single global hotkey that mutes your microphone(s) at the system level (the Windows Core Audio API) and applies to any and all applications capturing audio from your audio recording devices.
+
+With an on-screen display and strict error policy, mute gives you definitive knowledge of whether anything on your computer can hear you through any of your audio capture devices. If mute encounters an error, it will not tell you that you are muted. Only upon Windows reporting that all connected audio capture devices were successfully muted does mute show you that you are muted.
+
+Certain nice-to-have features have been intentionally left out to make mute safe to use with anti-cheat software. mute does not use any hooks.
+
+## How to install
+
+Download mute.exe in the Releases section. Double click to run. Delete to uninstall. The executable leaves no trace behind.
+
+## How to use
+
+After launching mute.exe, press *Alt+M* to mute and unmute your microphone(s). An on-screen display briefly shows whether your microphone(s) are muted or unmuted. Press Alt+Shift+M or double click mute.exe again to quit.
+
+## How does it work?
+
+mute is extremely fast and small. It is written in plain C and directly interfaces with Windows. It uses the Core Audio API to enumerate through all connected audio recording devices at system level and mutes or unmutes them all at the same time.
+
+This is equivalent to going to the Recording tab in the `mmsys.cpl` control panel, right clicking each microphone, Properties, Levels, and clicking the speaker icon to mute the recording device. In fact, you can go to that control panel and check that each audio recording device is muted and unmuted when you use mute.
+
+__NOTE:__ If you have a setup with multiple audio capture devices and need them to be in a mixed state of muted and unmuted, then this utility is not for you. mute values simplicity and the 80% use case of just wanting to be muted or unmuted and achieves this by muting and unmuting all connected audio capture devices all at once.
+
+## Limitations / Known issues
+
+- Features like hold-to-unmute are not implemented as they require a keyboard hook which may trip anti-cheat software
+- mute's OSD can not show over fullscreen applications (windowed fullscreen is fine!), as doing so would require a hook which may trip anti-cheat software
+- At the moment mute sticks to non-AA text drawing, which can sometimes look a little uneven, but is extremely fast and has a clean implementation
+- There is a bug related to special caching of layered windows where the OSD may briefly flash the prior muted/unmuted state. It is inconsequential, but will be fixed in time.
+
+## Donations
+
+For some reason this convenient functionality is neither a native part of Windows itself nor a use case Microsoft are willing to support (see PowerToys Video Conference Mute). In addition, it seems that there is almost as-if a sport in offering this functionality only with paid software and not for free. I'm unsure why.
+
+If you really appreciate me spending my time creating this and making it available free of charge, then consider donating to me on [Patreon](https://www.patreon.com/stianhoiland).
+
+## Buildling from source
+
+These instructions require `git`, `cmake`, and *Visual Studio* or *MSBuild*.
+1. `git clone https://github.com/stianhoiland/mute.git`
+2. `cd mute`
+3. `mkdir build`
+4. `cmake -G "Visual Studio 17 2022" -B build`
+5. *(in developer console)* `devenv build\mute.sln /build "MinSizeRel|x64"`
+6. or: *(in developer console)* `msbuild build\mute.sln /property:Configuration=MinSizeRel`
+7. *(run mute)* `build\MinSizeRel\mute.exe`
+
+
+## Credits
+
+My girlfriend who made the beautiful icon.
+
+Dennis Ritchie who co-created the C programming language. Rest in peace.
+
+## Endorsements
+
+> Another utility to rule them all,
+    another utility to find them,
+Another utility to bring them all
+    and in streaming bind them.
+~ Sauron feat. Stian
---
+++ src/mute.c
@@ -0,0 +1,266 @@
+﻿#define UNICODE
+#define WIN32_LEAN_AND_MEAN
+
+#include <WINDOWS.H>
+#include <COMBASEAPI.h>
+#include <MMDEVICEAPI.h>
+#include <ENDPOINTVOLUME.h>
+
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+static CONST GUID IID_IMMDeviceEnumerator = { 0xA95664D2, 0x9614, 0x4F35, { 0xA7, 0x46, 0xDE, 0x8D, 0xB6, 0x36, 0x17, 0xE6 } };
+static CONST GUID IID_MMDeviceEnumerator = { 0xBCDE0395, 0xE52F, 0x467C, { 0x8E, 0x3D, 0xC4, 0x57, 0x92, 0x91, 0x69, 0x2E } };
+static CONST GUID IID_IAudioEndpointVolume = { 0x5CDF2C82, 0x841E, 0x4546, { 0x97, 0x22, 0x0C, 0xF7, 0x40, 0x78, 0x22, 0x9A } };
+
+static BOOL AllMicrophonesMuted(BOOL toggle)
+{
+    IMMDeviceEnumerator *deviceEnumerator = NULL;
+    IMMDeviceCollection *deviceCollection = NULL;
+    UINT deviceCount = 0;
+    IMMDevice *device = NULL;
+    IAudioEndpointVolume *endpointVolume = NULL;
+    BOOL allMuted = FALSE;
+	// Accumulate HRESULTs and return false if not 100% sure that all microphones were muted
+	BOOL success = SUCCEEDED(CoCreateInstance(&IID_MMDeviceEnumerator, NULL, CLSCTX_INPROC_SERVER, &IID_IMMDeviceEnumerator, (VOID *)&deviceEnumerator));
+	if (success && deviceEnumerator) success = SUCCEEDED(deviceEnumerator->lpVtbl->EnumAudioEndpoints(deviceEnumerator, eCapture, DEVICE_STATE_ACTIVE, &deviceCollection));
+	if (success && deviceCollection) success = SUCCEEDED(deviceCollection->lpVtbl->GetCount(deviceCollection, &deviceCount));
+	if (success && deviceCount > 0) {
+        // Check if all audio capture devices are muted
+		for (INT i = 0 ; i < deviceCount && success; i++) {
+			device = NULL;
+			endpointVolume = NULL;
+            success = SUCCEEDED(deviceCollection->lpVtbl->Item(deviceCollection, i, &device));
+			if (success && device) success = SUCCEEDED(device->lpVtbl->Activate(device, &IID_IAudioEndpointVolume, CLSCTX_INPROC_SERVER, NULL, (VOID *)&endpointVolume));
+			if (success && endpointVolume) success = SUCCEEDED(endpointVolume->lpVtbl->GetMute(endpointVolume, &allMuted));
+			if (!allMuted) break;
+		}
+        if (toggle) {
+            // If any audio capture device is not muted, mute all audio capture devices; otherwise unmute all audio capture devices
+		    for (INT i = 0 ; i < deviceCount && success; i++) {
+			    device = NULL;
+			    endpointVolume = NULL;
+                success = SUCCEEDED(deviceCollection->lpVtbl->Item(deviceCollection, i, &device));
+			    if (success && device) success = SUCCEEDED(device->lpVtbl->Activate(device, &IID_IAudioEndpointVolume, CLSCTX_INPROC_SERVER, NULL, (VOID *)&endpointVolume));
+			    if (success && endpointVolume) success = SUCCEEDED(endpointVolume->lpVtbl->SetMute(endpointVolume, !allMuted, NULL));
+		    }
+		    if (success) allMuted = !allMuted;
+        }
+	}
+	if (endpointVolume) endpointVolume->lpVtbl->Release(endpointVolume);
+	if (device) device->lpVtbl->Release(device);
+	if (deviceCollection) deviceCollection->lpVtbl->Release(deviceCollection);
+	if (deviceEnumerator) deviceEnumerator->lpVtbl->Release(deviceEnumerator);
+    // Beware that FALSE is overloaded with two meanings:
+    //  1) Either not all audio capture devices are muted
+    //  2) Or there was an error determining whether all audio capture devices are muted
+    // Both cases of FALSE mean that some audio capture devices may be recording.
+    return success && (allMuted || !deviceCount);
+}
+
+typedef struct HOTKEY { UINT id, mod, key; } HOTKEY;
+
+static CONST HOTKEY Hotkey1 = { 1, MOD_ALT,             0x4D }; // Alt+M to toggle mute
+static CONST HOTKEY Hotkey2 = { 2, MOD_ALT | MOD_SHIFT, 0x4D }; // Alt+Shift+M to quit
+
+#define THEME3
+
+#if defined THEME1
+static CONST INT FontSize = 16;
+static CONST WCHAR FontName[] = L"Segoe UI";
+static CONST WCHAR MutedText[] = L"●";
+static CONST WCHAR UnmutedText[] = L"●";
+static CONST COLORREF MutedColor = RGB(64, 255, 64); // Green
+static CONST COLORREF UnmutedColor = RGB(255, 64, 64); // Red
+static CONST UINT TextPosition = DT_RIGHT | DT_TOP;
+#elif defined THEME2
+static CONST INT FontSize = 64;
+static CONST WCHAR FontName[] = L"Segoe UI";
+static CONST WCHAR MutedText[] = L"|●";
+static CONST WCHAR UnmutedText[] = L"●";
+static CONST COLORREF MutedColor = RGB(64, 255, 64); // Green
+static CONST COLORREF UnmutedColor = RGB(255, 64, 64); // Red
+static CONST UINT TextPosition = DT_LEFT | DT_TOP;
+#elif defined THEME3
+static CONST INT FontSize = 64;
+static CONST WCHAR FontName[] = L"Segoe UI";
+static CONST WCHAR MutedText[] = L"🔇MUTED"; // Some cool alternatives: ⏺⏸︎🔇🎙
+static CONST WCHAR UnmutedText[] = L"🎙LIVE";
+static CONST COLORREF MutedColor = RGB(128, 128, 128); // Gray
+static CONST COLORREF UnmutedColor = RGB(255, 64, 64); // Red
+static CONST UINT TextPosition = DT_CENTER | DT_VCENTER;
+#endif
+
+static CONST UINT ShowDuration = 1000; // Milliseconds
+
+static CONST DWORD WindowStyle = WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST | WS_EX_NOACTIVATE;
+static CONST WPARAM TimerID = 1;
+
+static HWND Window;
+static RECT Rect;
+static HDC Context;
+static HBITMAP Bitmap;
+static HFONT Font;
+static HPEN Pen;
+static WCHAR *Text; // Points to either MutedText or UnmutedText
+static COLORREF *TextColor; // Points to either MutedColor or UnmutedColor
+
+static VOID DrawProc(void)
+{
+    INT save = SaveDC(Context);
+    FillRect(Context, &Rect, (HBRUSH)GetStockObject(BLACK_BRUSH));
+    SelectObject(Context, Font);
+    SelectObject(Context, Pen);
+    SetBkMode(Context, TRANSPARENT);
+    SetTextColor(Context, TextColor);
+    BeginPath(Context);
+    DrawTextW(Context, Text, -1, &Rect, TextPosition | DT_SINGLELINE);
+    EndPath(Context);
+    StrokePath(Context);
+    DrawTextW(Context, Text, -1, &Rect, TextPosition | DT_SINGLELINE);
+    RestoreDC(Context, save);
+}
+
+static VOID ShowOSD(BOOL toggleMuted)
+{
+    // Check mic state (and toggle, if specified), then make OSD visible for 1 second
+    if (AllMicrophonesMuted(toggleMuted)) {
+        Text = MutedText;
+        TextColor = MutedColor;
+    } else {
+        Text = UnmutedText;
+        TextColor = UnmutedColor;
+    }
+    DrawProc();
+    InvalidateRect(Window, &Rect, TRUE);
+    ShowWindow(Window, SW_SHOW);
+    SetTimer(Window, TimerID, ShowDuration, NULL);
+    PAINTSTRUCT ps;
+    if (BeginPaint(Window, &ps)) {
+        BitBlt(ps.hdc, 0, 0, Rect.right - Rect.left, Rect.bottom - Rect.top, Context, 0, 0, SRCCOPY);
+        EndPaint(Window, &ps);
+    }
+}
+
+static VOID OnWindowCreate(HWND hwnd)
+{
+    Window = hwnd;
+    // Clear all window styles for very plain window
+    SetWindowLongW(Window, GWL_STYLE, 0);
+    // Position at the center of the primary monitor
+    MONITORINFO mi = {sizeof mi};
+    GetMonitorInfoW(MonitorFromPoint((POINT){0, 0}, MONITOR_DEFAULTTOPRIMARY), &mi);
+    // Asynchronously calls WM_SIZE:
+    SetWindowPos(Window, HWND_TOPMOST, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_HIDEWINDOW);
+    // Create font & pen
+    Font = CreateFontW(FontSize, 0, 0, 0, FW_EXTRABOLD, FALSE, FALSE, FALSE, 0, 0, 0, ANTIALIASED_QUALITY, 0, FontName);
+    Pen = CreatePen(PS_SOLID, 4, RGB(1, 1, 1));
+
+    if (TRUE) {
+        // Make BLACK the transparency color
+        SetLayeredWindowAttributes(Window, RGB(0, 0, 0), 0, LWA_COLORKEY);
+    } else {
+        // I need to find the correct way to use UpdateLayeredWindow instead of SetLayeredWindowAttributes.
+        // The reason is that drawing of layered windows work differently and may often briefly flash a
+        // cached graphical output upon showing the window, which doesn't look too good.
+        RECT cr;
+        RECT wr;
+        GetClientRect(hwnd, &cr);
+        GetWindowRect(hwnd, &wr);
+        int W = wr.right - wr.left;
+        int H = wr.bottom - wr.top;
+        HDC hsdc = GetDC(NULL); // Note that this is not GetDC(hwnd) because UpdateLayeredWindow requires DC of screen
+        HDC hdc  = GetDC(hwnd);
+        HDC hbdc = CreateCompatibleDC(hdc); // Should create from window DC rather than screen DC 
+        HBITMAP bmp = CreateCompatibleBitmap(hsdc, W, H);
+        HGDIOBJ oldbmp = SelectObject(hbdc, (HGDIOBJ)bmp);
+        FillRect(hbdc, &cr, (HBRUSH)GetStockObject(BLACK_BRUSH));
+        UpdateLayeredWindow(hwnd, hsdc, &(POINT){wr.left, wr.top}, &(SIZE){W, H}, hbdc, &(POINT){0, 0}, RGB(0, 0, 0), NULL, ULW_COLORKEY);
+        SelectObject(hbdc, oldbmp);
+        DeleteObject(bmp);
+        DeleteDC(hbdc);
+        ReleaseDC(hwnd, hdc);
+        ReleaseDC(NULL, hsdc);
+    }
+    
+    // Show OSD initially to inform mic state and indicate program started
+    ShowOSD(FALSE);
+}
+
+static VOID OnWindowDestroy(void)
+{
+    DeleteObject(Bitmap);
+    DeleteDC(Context);
+    DeleteObject(Pen);
+    DeleteObject(Font);
+}
+
+static VOID OnWindowResize(void)
+{
+    RECT rect;
+    GetClientRect(Window, &rect);
+    if (!EqualRect(&Rect, &rect)) {
+        DeleteObject(Bitmap);
+        DeleteDC(Context);
+        HDC context = GetDC(Window);
+        Rect = rect;
+        Context = CreateCompatibleDC(context);
+        Bitmap = CreateCompatibleBitmap(context, Rect.right - Rect.left, Rect.bottom - Rect.top);
+        DeleteObject(SelectObject(Context, Bitmap));
+        ReleaseDC(Window, context);
+    }
+}
+
+static VOID OnHotkey(WPARAM id)
+{
+    if (id == Hotkey1.id) {
+        ShowOSD(TRUE);
+    }
+    if (id == Hotkey2.id) {
+        if (MessageBoxW(NULL, L"Do you want to quit mute?", L"mute", MB_OKCANCEL | MB_ICONINFORMATION | MB_SYSTEMMODAL) == IDOK) {
+            PostMessageW(Window, WM_SYSCOMMAND, SC_CLOSE, 0);
+        }
+    }
+}
+
+static VOID OnTimer(WPARAM id)
+{
+    ShowWindow(Window, SW_HIDE);
+    KillTimer(Window, id);
+}
+
+static LRESULT WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+    switch (msg) {
+    case WM_CREATE:  OnWindowCreate(hwnd); break;
+    case WM_DESTROY: OnWindowDestroy();    break;
+    case WM_SIZE:    OnWindowResize();     break;
+    case WM_HOTKEY:  OnHotkey(wparam);     break;
+    case WM_TIMER:   OnTimer(wparam);      break;
+    }
+    return DefWindowProcW(hwnd, msg, wparam, lparam);
+}
+
+INT wWinMain(HINSTANCE instance, HINSTANCE previnstance, WCHAR *args, INT show)
+{
+    static CONST WCHAR *class = L"StiansMuteUtilityClass";
+    HANDLE mutex = CreateMutexW(NULL, 0, class);
+    if (GetLastError() == ERROR_ALREADY_EXISTS) {
+        if (MessageBoxW(NULL, L"Do you want to quit mute?", L"mute", MB_OKCANCEL | MB_ICONINFORMATION | MB_SYSTEMMODAL) == IDOK) {
+            PostMessageW(FindWindowExW(NULL, NULL, class, NULL), WM_SYSCOMMAND, SC_CLOSE, 0);
+        }
+        ExitProcess(0);
+    }
+    WNDCLASSEXW wcex = {0};
+    wcex.cbSize = sizeof wcex;
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = instance;
+    wcex.lpszClassName = class;
+    MSG msg = {0};
+    BOOL com = SUCCEEDED(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE)); // Init COM
+    HWND hwnd = CreateWindowExW(WindowStyle, RegisterClassExW(&wcex), NULL, WS_POPUP, 0, 0, 0, 0, NULL, NULL, instance, NULL);
+	BOOL hotkey = RegisterHotKey(hwnd, Hotkey1.id, Hotkey1.mod | MOD_NOREPEAT, Hotkey1.key) && RegisterHotKey(hwnd, Hotkey2.id, Hotkey2.mod | MOD_NOREPEAT, Hotkey2.key);
+	if (com && hwnd && hotkey) while (GetMessageW(&msg, hwnd, 0, 0) > 0) DispatchMessageW(&msg);
+	if (hotkey) UnregisterHotKey(hwnd, Hotkey1.id) && UnregisterHotKey(hwnd, Hotkey2.id);
+	if (com) CoUninitialize();
+	return msg.wParam;
+}
